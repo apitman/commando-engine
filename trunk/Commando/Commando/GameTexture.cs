dfee8a341344022e3dce32091ef7dec92aa75929
jj3326 <jj3326@c4092a10-dea3-11dd-87d8-31aa04531ed5>
2009-01-15 16:16:48 +0000

    Made AnimationSet and GameTexture functional

--- trunk/Commando/Commando/GameTexture.cs
+++ trunk/Commando/Commando/GameTexture.cs
@@ -39,41 +39,89 @@ namespace Commando
 
         //Each Vector4 is the bounds (x of top right corner, y of top right corner,
         //width, height) of each individual frame, or image in this texture
-        protected Vector4[] imageDimensions_;
+        protected Rectangle[] imageDimensions_;
+
+        protected SpriteBatch spriteBatch_;
 
         public GameTexture()
         {
+
         }
 
-        public GameTexture(string filename)
+        public GameTexture(string filename, SpriteBatch spriteBatch, GraphicsDevice graphics)
         {
+            //TODO: add functionality to read from a file to get imageDims
+
+            spriteBatch_ = spriteBatch;
+
+            texture_ = Texture2D.FromFile(graphics, filename);
+
+            imageDimensions_ = new Rectangle[1];
+            imageDimensions_[0] = new Rectangle(0, 0, texture_.Width, texture_.Height);
+        }
+
+        public GameTexture(GameTexture gTexture)
+        {
+            spriteBatch_ = gTexture.spriteBatch_;
+
+            texture_ = gTexture.texture_;
+
+            Array.Copy(gTexture.imageDimensions_, imageDimensions_, gTexture.imageDimensions_.GetLength(0));
         }
 
         public Texture2D getTexture()
         {
-
-            return null;
+            return texture_;
         }
 
         public void setTexture(Texture2D tex)
         {
+            texture_ = tex;
         }
 
-        public Vector4[] getImageDimensions()
+        public Rectangle[] getImageDimensions()
         {
-
-            return new Vector4[1];
+            return imageDimensions_;
         }
 
-        public void setImageDimensions(Vector4[] dims)
+        public void setImageDimensions(Rectangle[] dims)
         {
+            Array.Copy(dims, imageDimensions_, dims.GetLength(0));
+        }
+
+        public uint getNumberOfImages()
+        {
+            return imageDimensions_.GetLength(0);
+        }
+
+        public SpriteBatch getSpriteBatch()
+        {
+            return spriteBatch_;
+        }
+
+        public void setSpriteBatch(SpriteBatch spriteBatch)
+        {
+            spriteBatch_ = spriteBatch;
         }
 
         //preconditions: texture_ and imageDimensions_ are not NULL
         //postconditions: the specified image is drawn to the screen at origin with the
         //  specified rotation and depth.
-        public void drawImage(uint imageNumber, Vector2 origin, float rotation, float depth)
+        public void drawImage(uint imageNumber, Vector2 position, float rotation, float depth)
         {
+            Vector2 originOfImage = new Vector2(((float)imageDimensions_[imageNumber].Width) / 2.0f, ((float)imageDimensions_[imageNumber].Height) / 2.0f);
+            spriteBatch_.Draw(texture_, position, imageDimensions_[imageNumber], Color.White, rotation, originOfImage, SpriteEffects.None, depth);
+        }
+
+        public void drawImageWithDim(uint imageNumber, Rectangle destinationDims, float rotation, float depth)
+        {
+            Vector2 originOfImage = new Vector2(((float)imageDimensions_[imageNumber].Width) / 2.0f, ((float)imageDimensions_[imageNumber].Height) / 2.0f);
+            spriteBatch_.Draw(texture_, destinationDims, imageDimensions_[imageNumber], Color.White, rotation, originOfImage, SpriteEffects.None, depth);
+        }
+
+        public void drawImageWithDim(uint imageNumber, Rectangle destinationDims, float depth)
+        {
+            spriteBatch_.Draw(texture_, destinationDims, imageDimensions_[imageNumber], Color.White, 0.0f, Vector2.Zero, SpriteEffects.None, depth);
         }
     }
 }
