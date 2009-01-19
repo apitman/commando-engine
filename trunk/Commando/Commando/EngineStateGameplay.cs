9805e707c9b9f7c480aac86cb4aac16a17e49993
drewpit@gmail.com <drewpit@gmail.com@c4092a10-dea3-11dd-87d8-31aa04531ed5>
2009-01-19 04:28:04 +0000

    Added text to more clearly define the health bar and also to display the amount of ammo left.

--- trunk/Commando/Commando/EngineStateGameplay.cs
+++ trunk/Commando/Commando/EngineStateGameplay.cs
@@ -94,6 +94,9 @@ namespace Commando
             healthBar_.draw(new GameTime());
             TextureMap.getInstance().getTexture("healthBarOutline").drawImage(0, healthBarPos_, 0.0f, 0.8f);
             weapon_.draw(new GameTime());
+
+            FontMap.getInstance().getFont("Kootenay").drawString("Health", new Vector2(healthBarPos_.X - 27.0f, healthBarPos_.Y - 12.0f), Color.Black);
+            FontMap.getInstance().getFont("Kootenay").drawString("20/20 bullets", new Vector2(weaponPos_.X + 50.0f, weaponPos_.Y - 14.0f), Color.Black);
         }
 
         #endregion
