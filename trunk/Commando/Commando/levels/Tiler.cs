1118007d8fdbbbd4428779a249fbb7a6b2c6cdd6
jj3326 <jj3326@c4092a10-dea3-11dd-87d8-31aa04531ed5>
2009-01-19 18:45:59 +0000

    Added a Tiler class

---
+++ trunk/Commando/Commando/levels/Tiler.cs
@@ -0,0 +1,46 @@
+﻿/*
+***************************************************************************
+* Copyright 2009 Eric Barnes, Ken Hartsook, Andrew Pitman, & Jared Segal  *
+*                                                                         *
+* Licensed under the Apache License, Version 2.0 (the "License");         *
+* you may not use this file except in compliance with the License.        *
+* You may obtain a copy of the License at                                 *
+*                                                                         *
+* http://www.apache.org/licenses/LICENSE-2.0                              *
+*                                                                         *
+* Unless required by applicable law or agreed to in writing, software     *
+* distributed under the License is distributed on an "AS IS" BASIS,       *
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*
+* See the License for the specific language governing permissions and     *
+* limitations under the License.                                          *
+***************************************************************************
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Commando.objects;
+using Microsoft.Xna.Framework;
+
+namespace Commando.levels
+{
+    static class Tiler
+    {
+        public const int tileSideLength_ = 15;
+
+        public static List<TileObject> getTiles(int[,] tiles)
+        {
+            List<TileObject> retList = new List<TileObject>();
+            for (int y = 0; y < tiles.GetLength(0); y++)
+            {
+                for (int x = 0; x < tiles.GetLength(1); x++)
+                {
+                    retList.Add(new TileObject(TextureMap.getInstance().getTexture("Tile_" + tiles[y, x]), new Vector2((float)x * tileSideLength_, (float)y * tileSideLength_), Vector2.Zero, 0.0f));
+                }
+            }
+            return retList;
+        }
+    }
+}
