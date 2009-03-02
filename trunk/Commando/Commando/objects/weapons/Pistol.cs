47d715872b73cccc89d28b865c06c4fba662fef7
ken.a.hartsook <ken.a.hartsook@c4092a10-dea3-11dd-87d8-31aa04531ed5>
2009-03-02 22:26:41 +0000

    - Refactored Projectile to be abstract - Created concrete implementation Bullet, much simpler to use - Modified existing weapons to use Bullet

--- trunk/Commando/Commando/objects/weapons/Pistol.cs
+++ trunk/Commando/Commando/objects/weapons/Pistol.cs
@@ -1,4 +1,22 @@
-﻿using System;
+﻿/*
+***************************************************************************
+* Copyright 2009 Eric Barnes, Ken Hartsook, Andrew Pitman, & Jared Segal  *
+*                                                                         *
+* Licensed under the Apache License, Version 2.0 (the "License");         *
+* you may not use this file except in compliance with the License.        *
+* You may obtain a copy of the License at                                 *
+*                                                                         *
+* http://www.apache.org/licenses/LICENSE-2.0                              *
+*                                                                         *
+* Unless required by applicable law or agreed to in writing, software     *
+* distributed under the License is distributed on an "AS IS" BASIS,       *
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*
+* See the License for the specific language governing permissions and     *
+* limitations under the License.                                          *
+***************************************************************************
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,11 +29,13 @@ namespace Commando.objects.weapons
 {
     class Pistol : WeaponAbstract
     {
+        protected const string TEXTURE_NAME = "Pistol";
+
         protected GameTexture laserImage_;
         protected Vector2 laserTarget_;
 
-        public Pistol(List<DrawableObjectAbstract> pipeline, CharacterAbstract character, GameTexture animation, Vector2 gunHandle)
-            : base(pipeline, character, animation, gunHandle)
+        public Pistol(List<DrawableObjectAbstract> pipeline, CharacterAbstract character, Vector2 gunHandle)
+            : base(pipeline, character, TextureMap.fetchTexture(TEXTURE_NAME), gunHandle)
         {
             laserImage_ = TextureMap.fetchTexture("laserpointer");
         }
@@ -31,7 +51,7 @@ namespace Commando.objects.weapons
                 points.Add(new Vector2(2f, -2f));
                 rotation_.Normalize();
                 Vector2 pos = position_ + rotation_ * 15f;
-                Projectile bullet = new Projectile(TextureMap.getInstance().getTexture("Bullet"), detector, new ConvexPolygon(points, Vector2.Zero), 2.5f, rotation_ * 20.0f, pos, rotation_, 0.5f);
+                Bullet bullet = new Bullet(detector, pos, rotation_);
                 drawPipeline_.Add(bullet);
                 refireCounter_ = 10;
                 character_.getAmmo().update(character_.getAmmo().getValue() - 1);
